68edbe85d4aa69b7edf50eca5f8acc82a19cbff6
amosbastian <amosbastian@gmail.com>
2018-11-13 20:04:13 +0000

    Add manager's team to toggleable div

--- src/js/classicLeague.js
+++ src/js/classicLeague.js
@@ -88,17 +88,105 @@ async function getClassicLeague() {
   throw new Error(response.status);
 }
 
-function toggleTeam() {
-  const teamRow = this.parentElement.parentElement.nextElementSibling;
-
-  if (this.textContent === 'Show team') {
-    this.textContent = 'Hide team';
-  } else {
-    this.textContent = 'Show team';
+/**
+ * Returns an array of all players playing for teams in the Premier League.
+ */
+async function getPlayers() {
+  const response = await fetch('https://fantasy.premierleague.com/drf/elements/');
+  if (response.status === 200) {
+    const json = await response.json();
+    return json;
   }
 
+  throw new Error(response.status);
+}
+
+function getIdFromRow(row) {
+  const rowData = row.getElementsByTagName('td');
+  const managerURL = rowData[1].getElementsByTagName('a')[0].href;
+  const managerId = managerURL.split('/').pop();
+  return managerId;
+}
+
+async function createTeamDiv(picks) {
+  const starterIds = picks.reduce((playerIds, player) => {
+    if (player.position <= 11) {
+      playerIds.push(player.element);
+    }
+    return playerIds;
+  }, []);
+
+  console.log(starterIds);
+
+  const benchIds = picks.reduce((playerIds, player) => {
+    if (player.position > 11) {
+      playerIds.push(player.element);
+    }
+    return playerIds;
+  }, []);
+
+  const allPlayers = await getPlayers();
+  const startingPlayers = allPlayers.filter(player => starterIds.includes(player.id));
+  console.log(startingPlayers);
+  const benchPlayers = allPlayers.filter(player => benchIds.includes(player.id));
+
+  const teamDiv = document.createElement('div');
+  teamDiv.className = 'manager-team';
+
+  const startersDiv = document.createElement('div');
+  startersDiv.className = 'manager-team--left';
+  const startersHeader = document.createElement('h5');
+  startersHeader.textContent = 'Starters';
+  startersHeader.style.textAlign = 'right';
+  startersDiv.appendChild(startersHeader);
+
+  const benchDiv = document.createElement('div');
+  benchDiv.className = 'manager-team--right';
+  const benchHeader = document.createElement('h5');
+  benchHeader.textContent = 'Bench';
+  benchHeader.style.textAlign = 'left';
+  benchDiv.appendChild(benchHeader);
+
+  startingPlayers
+    .sort((a, b) => (starterIds.indexOf(a.id) > starterIds.indexOf(b.id) ? 1 : -1))
+    .forEach((player) => {
+      const playerText = document.createElement('p');
+      playerText.style.textAlign = 'right';
+      playerText.textContent = player.web_name;
+      startersDiv.appendChild(playerText);
+    });
+
+  benchPlayers
+    .sort((a, b) => (benchIds.indexOf(a.id) > benchIds.indexOf(b.id) ? 1 : -1))
+    .forEach((player) => {
+      const playerText = document.createElement('p');
+      playerText.style.textAlign = 'left';
+      playerText.textContent = player.web_name;
+      benchDiv.appendChild(playerText);
+    });
+
+  teamDiv.appendChild(startersDiv);
+  teamDiv.appendChild(benchDiv);
+  return teamDiv;
+}
+
+async function toggleTeam() {
+  const parentRow = this.parentElement.parentElement;
+  const teamRow = parentRow.nextElementSibling;
+
   this.classList.toggle('button-toggle-team--active');
-  teamRow.classList.toggle('manager-team--hidden');
+  teamRow.classList.toggle('manager-team-row--hidden');
+
+  if (this.textContent === 'Hide team') {
+    this.textContent = 'Show team';
+    return;
+  }
+  this.textContent = 'Hide team';
+
+  const currentGameweek = await getCurrentGameweek();
+  const managerId = getIdFromRow(parentRow);
+  const managerPicks = await getUserPicks(managerId, currentGameweek);
+  teamRow.getElementsByTagName('td')[0].appendChild(await createTeamDiv(managerPicks.picks));
 }
 
 /**
@@ -111,9 +199,7 @@ async function updateLeagueTable() {
 
   const managers = await Promise.all(managerIds.map(async (managerId) => {
     const manager = await getUser(managerId);
-    const managerPicks = await getUserPicks(managerId, currentGameweek);
     // const managerHistory = await getUserHistory(managerId);
-    manager.picks = managerPicks;
     // manager.history = managerHistory;
     return manager;
   }));
@@ -132,7 +218,7 @@ async function updateLeagueTable() {
     toggleTeamButton.addEventListener('click', toggleTeam);
 
     const newRow = tableBody.insertRow(row.rowIndex);
-    newRow.className = 'manager-team manager-team--hidden';
+    newRow.className = 'manager-team-row manager-team-row--hidden';
     newRow.innerHTML = `
       <td colspan="${row.getElementsByTagName('td').length}">
       </td>
