16cf6dd449dfafd55ccebef1818569f39cba393d
amosbastian <amosbastian@gmail.com>
2018-11-25 00:03:09 +0000

    Add login button

--- src/js/popup.js
+++ src/js/popup.js
@@ -7,3 +7,14 @@ document.addEventListener('DOMContentLoaded', () => {
     link.onclick = () => chrome.tabs.create({ active: true, url: location });
   });
 });
+
+function login() {
+  const email = document.getElementById('fpl-email').value;
+  const password = document.getElementById('fpl-password').value;
+  console.log(email, password);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const loginButton = document.getElementById('fpl-login');
+  loginButton.onclick = login;
+});
