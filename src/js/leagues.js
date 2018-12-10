0115f130f2e42e1b4ed9a05b7922738a72c668bc
amosbastian <amosbastian@gmail.com>
2018-12-10 22:50:29 +0000

    Improve toggling league table

--- src/js/leagues.js
+++ src/js/leagues.js
@@ -1,20 +1,30 @@
 import '../css/main.scss';
 
-const triangles = Array.from(document.getElementsByClassName('icon-triangle-right'));
+const leagueTriangles = Array.from(document.getElementsByClassName('icon-triangle-right leagues'));
+const leagueTables = Array.from(document.getElementsByClassName('fpl-league-table-container'));
 
 function back() {
   chrome.browserAction.setPopup({ popup: 'index.html' });
   window.location.href = 'index.html';
 }
 
-function showLeague() {
-  triangles.forEach(triangle => triangle.classList.remove('active'));
+function showLeagueTable() {
+  if (this.classList.contains('active')) {
+    this.classList.remove('active');
+    this.parentElement.nextElementSibling.classList.remove('active');
+    return;
+  }
+
+  leagueTriangles.forEach(triangle => triangle.classList.remove('active'));
   this.classList.toggle('active');
+
+  leagueTables.forEach(leagueTable => leagueTable.classList.remove('active'));
+  this.parentElement.nextElementSibling.classList.toggle('active');
 }
 
 document.addEventListener('DOMContentLoaded', () => {
   const backButton = document.getElementById('back');
   backButton.addEventListener('click', back);
 
-  triangles.forEach(triangle => triangle.addEventListener('click', showLeague));
+  leagueTriangles.forEach(triangle => triangle.addEventListener('click', showLeagueTable));
 });
