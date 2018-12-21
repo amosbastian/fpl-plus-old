0c1ab50ee5165bd66b2fea01ac8029388b3c2d4f
amosbastian <amosbastian@gmail.com>
2018-12-21 02:04:39 +0000

    Implement fixture dropdown JavaScript

--- src/js/fixtures.js
+++ src/js/fixtures.js
@@ -7,13 +7,19 @@ function showFixtures() {
   const fixtureTriangles = Array.from(document.getElementsByClassName('icon-triangle-right fixtures'));
   if (this.classList.contains('active')) {
     this.classList.remove('active');
-    this.parentElement.nextElementSibling.classList.remove('active');
+    this.parentElement.nextElementSibling.classList.add('hidden');
     return;
   }
 
   // Dropdown triangles
   fixtureTriangles.forEach(triangle => triangle.classList.remove('active'));
   this.classList.toggle('active');
+
+  const fixtureContainers = Array.from(document.getElementsByClassName('fixtures-container'));
+  fixtureContainers.forEach(container => container.classList.add('hidden'));
+
+  const currentContainer = this.parentElement.nextElementSibling;
+  currentContainer.classList.toggle('hidden');
 }
 
 document.addEventListener('DOMContentLoaded', () => {
